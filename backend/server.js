a67961ba7a26ae6d543d555c41904cb25e55cfbf
alfardan59 <alfardanarfin500@gmail.com>
2025-01-31 14:34:13 +0530

    Backend setup and signout done

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
+
+const app=express();
+const PORT=process.env.PORT||3000;
+
+app.use(express.json()); //allows to parse the body of the request
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT,()=>{
+    console.log("Server is running on port http://localhost:"+PORT);
+    connectDB();
+});
+    
