15fa42b45708b89a71d427d948535d3d0129934e
alfardan59 <alfardanarfin500@gmail.com>
2025-02-03 19:05:47 +0530

    User and Product Route Created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
+import productRoutes from './routes/product.route.js';
+
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
@@ -10,8 +13,10 @@ const app=express();
 const PORT=process.env.PORT||3000;
 
 app.use(express.json()); //allows to parse the body of the request
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/products",productRoutes);
 
 app.listen(PORT,()=>{
     console.log("Server is running on port http://localhost:"+PORT);
